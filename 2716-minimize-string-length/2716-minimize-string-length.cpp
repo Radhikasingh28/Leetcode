ff452382e0310a03b378ba6f0bee888615b456b9
Radhika singh <radhikasingh90680@gmail.com>
2026-06-23 12:39:20 -0700

    Time: 14 ms (63.95%), Space: 16.9 MB (58.91%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimizedStringLength(string s) {
+        unordered_set<char> us ;
+        
+        for(char ch : s){
+            us.insert(ch);
+            
+        }
+        return us.size();
+        
+    }
+};
